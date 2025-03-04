0a2b6b37571eed714eb4f15f22377c9d009ae316
ViktorSivek <randomucet02@gmail.com>
2025-03-04 01:25:58 +0100

    style: Refactor SectionWrapper component formatting and imports

--- components/hoc/SectionWrapper.tsx
+++ components/hoc/SectionWrapper.tsx
@@ -1,15 +1,18 @@
 'use client'
 
-import { motion } from "framer-motion";
-import { staggerContainer } from "@/lib/motion";
+import { motion } from 'framer-motion'
+import { staggerContainer } from '@/lib/motion'
 
-export const SectionWrapper = (Component: React.ComponentType, idName: string) => {
+export const SectionWrapper = (
+  Component: React.ComponentType,
+  idName: string
+) => {
   function HOC() {
     return (
       <motion.section
         variants={staggerContainer()}
-        initial='hidden'
-        whileInView='show'
+        initial="hidden"
+        whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
         className="max-w-7xl mx-auto relative z-0 px-6 py-10 md:py-16"
       >
@@ -19,10 +22,10 @@ export const SectionWrapper = (Component: React.ComponentType, idName: string) =
 
         <Component />
       </motion.section>
-    );
+    )
   }
-  
-  return HOC;
-};
 
-export default SectionWrapper;
+  return HOC
+}
+
+export default SectionWrapper
